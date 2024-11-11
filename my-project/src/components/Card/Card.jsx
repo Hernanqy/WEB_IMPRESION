f3466c82a1ca8eb9a85710dd9990251202a1e44b
hernan gustavo quiroga <desarrolladoryotti@gmail.com>
2024-11-11 14:56:50 -0300

    maceta

--- my-project/src/components/Card/Card.jsx
+++ my-project/src/components/Card/Card.jsx
@@ -18,7 +18,7 @@ const Card = () => {
         <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
           <img src={pro1} alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
         </div>
-        <h3 className="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
+        <h3 className="mt-4 text-sm text-gray-700">Maceta</h3>
         <p className="mt-1 text-lg font-medium text-gray-900">$48</p>
       </a>
       <a href="#" className="group">
